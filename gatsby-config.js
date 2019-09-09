ad6c5d17766d6e1b6294a01efcdf6ba895b96143
illulli <illu.development@gmail.com>
2019-09-09 11:08:55 +1000

    chanegd the website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Mathis Runner App',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A website for the upcoming runner app for charity',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
